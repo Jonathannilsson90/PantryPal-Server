7523c23d4d864765b40b9118671f85a6ee8b52c9
Jonathan Nilsson <koffeinmissbrukarn@gmail.com>
2023-07-27 10:16:48 +0200

    Cleanup - removed unfinished code

--- src/routes/userRoute.ts
+++ src/routes/userRoute.ts
@@ -4,14 +4,10 @@ const router = express.Router();
 
 router.post("/register", userController.registerNewUser);
 router.post("/token", userController.generateAccessToken);
-router.delete("/", userController.deleteUser);
+router.delete("/delete", userController.deleteUser);
 
 router.post("/addToGroceryList", userController.addToGroceryList);
 router.get("/groceryList", userController.getGroceryList);
 router.delete("/deleteGroceryItem/:itemId", userController.removeFromGroceryList)
-
-
-/* router.put("/addLiked", userController.addLikedRecipe);
-router.put("/removeLiked", userController.removeLikedRecipe); */
 
 export default router;
